49c17635deb00aa6e56d45413ecbcf03b2c914c2
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-04-16 11:24:07 +0100

    working code that meets all current criteria

--- src/main/scala/CountOdd.scala
+++ src/main/scala/CountOdd.scala
@@ -1,5 +1,8 @@
 object CountOdd extends App {
 
-  def apply(input: Int) : Int = 0
+  def apply(input: Int) : Int = {
+    val allNumbers = (1 until input).toList
+    allNumbers.count(x => x % 2 != 0)
+  }
 
 }
--- src/test/scala/CountOddSpec.scala
+++ src/test/scala/CountOddSpec.scala
@@ -8,7 +8,17 @@ class CountOddSpec extends WordSpec with MustMatchers {
       CountOdd(1) mustEqual 0
     }
 
+    "return 1 when given 2" in {
+      CountOdd(2) mustEqual 1
+    }
 
+    "return 3 when given 7" in {
+      CountOdd(7) mustEqual 3
+    }
+
+    "return 4 when given 8" in {
+      CountOdd(8) mustEqual 4
+    }
   }
 
 }
